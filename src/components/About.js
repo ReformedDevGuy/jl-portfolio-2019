2b823510b149ab50320e5da602e1230629d4383d
John Larson <john@jjlarson.com>
2019-11-18 17:15:02 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,14 @@ const About = props => {
     <div className="about">
       <h2>John Larson, Web Developer</h2>
       <p>
-        I am a creative web developer with a focus on front-end web technologies.
-        My passion for Web Development started in 2009 when I first attended college, 
-        and since then I have been driven towards learning as much as I can in order 
-        to create beautiful and efficent websites and web applications for small 
-        businesses and individuals in need of an online presence. Over the past 10 
-        years I have taken a self-learning path to increase my skills and knowledge 
-        of the latest technologies, and recently I enrolled in classes to finish my 
-        degree. The main technologies I utilize are Angular and React, and I incorporate 
-        Bootstrap and Angular Material Design when necessary to ensure compatibility 
-        across all platforms.
+        My passion for Web Development started in 2009 and since then I have 
+        utilized both college courses and self-taught methods in order 
+        to create beautiful and efficent web solutions for small 
+        businesses and individuals in need of an online presence. The main technologies 
+        I utilize are Angular and React, and I incorporate Bootstrap and Angular Material 
+        Design when necessary to ensure compatibility across all platforms.
         </p>
+        <br />
         <p>
         Despite focusing on front-end technologies, I have extensive experience working 
         with back-end frameworks and databases, including NodeJS, Express, PHP, and Python. 
