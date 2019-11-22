9a691d1e68b8c10f194ee129be365ba5080e8096
John Larson <john@jjlarson.com>
2019-11-22 02:14:14 -0600

    Fixed images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-import Logo from "./img/favicon.png";
-
 const Footer = props => {
   return (
     <div className="footer">
       Copyright John Larson &copy; {new Date().getFullYear()}
-      
-      
-          <img src={Logo} alt="John Larson"></img>
-       
     </div>
-    
   );
 };
 
