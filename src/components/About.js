0fab8ceac04a266623aef8181481436a10703512
John Larson <john@jjlarson.com>
2019-11-22 10:06:36 -0600

    Updated About Secion, Correcting Syntax

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,25 @@ const About = props => {
     <div className="about">
       <h2>John Larson, Web Developer</h2>
       <p>
-        My passion for Web Development started in 2009, and since then I have 
-        utilized both college courses and self-taught methods in order 
-        to create beautiful and efficent web solutions for small 
-        businesses and individuals in need of an online presence. The main technologies 
-        I utilize are Angular and React, and I incorporate Bootstrap and Angular Material 
+        My passion for Web Development started in 2009, and since then I have
+        utilized both college courses and self-taught methods in order
+        to create beautiful and efficent web solutions for small
+        businesses and individuals in need of an online presence. The main technologies
+        I utilize are Angular and React, and I incorporate Bootstrap and Angular Material
         Design when necessary to ensure compatibility across all platforms.
         </p>
-        <br />
-        <p>
-        Despite focusing on front-end technologies, I have extensive experience working 
-        with back-end frameworks and databases, including NodeJS, Express, PHP, and Python. 
-        I have experience with SQL databases, like MySQL, Microsoft SQL Server, Percona and 
-        MariaDB, and I also frequently utilize NoSQL databases like MongoDB and CouchDB. My 
-        experience in Network Administration also means I am able to utilize server technologies 
-        such as IIS on Windows Servers, Apache on Ubuntu and Debian servers.
+      <br />
+      <p>
+        Despite focusing on front-end technologies, I have extensive experience working
+        with back-end frameworks and databases, including NodeJS, Express, PHP, and Python.
+        I have experience with SQL databases, like MySQL, Microsoft SQL Server, Percona and
+        MariaDB, and I also frequently utilize NoSQL databases like MongoDB and CouchDB. My
+        experience in Network Administration also means I am able to utilize server technologies
+        such as IIS on Windows Servers and Apache on Linux servers.
       </p>
       <br />
       <p>
-        My skills include: HTML5, CSS3, SASS, JavaScript, Git, React, Redux, Angular, 
+        My skills include: HTML5, CSS3, SASS, JavaScript, Git, React, Redux, Angular,
         Bootstrap, NodeJS, Express, MongoDB, PHP and SQL
       </p>
     </div>
