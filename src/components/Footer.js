b6836bdb7654e6b9b1dcf44fab52023ec86481a2
John Larson <john@jjlarson.com>
2019-11-22 02:09:05 -0600

    Meta changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Logo from "./img/favicon.png";
+
 const Footer = props => {
   return (
     <div className="footer">
       Copyright John Larson &copy; {new Date().getFullYear()}
+      
+      <Fade>
+          <img src={Logo} alt="John Larson"></img>
+        </Fade>
     </div>
+    
   );
 };
 
