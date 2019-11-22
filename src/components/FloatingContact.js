3fcf228b89d389618c27087279794d6c0fd434d6
John Larson <john@jjlarson.com>
2019-11-22 04:59:39 -0600

    Tweaked mobile responsiveness

--- src/components/FloatingContact.js
+++ src/components/FloatingContact.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import myResume from '../John Larson Resume.pdf';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import {  faFilePdf, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FaGithubSquare, FaLinkedin, FaFilePdf, FaRegEnvelope } from 'react-icons/fa';
+
 
 const FloatingContact = props => {
   return (
@@ -10,19 +9,19 @@ const FloatingContact = props => {
     <div className="icon-floats">
       <div>
         <a href="mailto:john@jjlarson.com" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faEnvelope} size="2x" className="contact-me-button-alt"></FontAwesomeIcon>
+          <FaRegEnvelope  className="contact-me-button" />
         </a>
         </div><div>
         <a href="https://www.linkedin.com/in/johnjlarson/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" className="contact-me-button"></FontAwesomeIcon>
+        <FaLinkedin className="contact-me-button"/>
         </a>
         </div><div>
         <a href="https://github.com/ReformedDevGuy" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithubSquare} size="2x" className="contact-me-button"></FontAwesomeIcon>
+        <FaGithubSquare className="contact-me-button"/>
         </a>
         </div><div>
         <a href={myResume} target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFilePdf} size="2x" className="contact-me-button-alt-2"></FontAwesomeIcon>
+        <FaFilePdf className="contact-me-button"/>
         </a>
         </div>
      </div>
